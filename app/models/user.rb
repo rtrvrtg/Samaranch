9b953672735a0bc012e64768e32560ad40de757a
Geoffrey Roberts <g.roberts@blackicemedia.com>
2012-03-20 00:52:29 +1100

    Removed a bunch of stuff from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,24 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+  #acts_as_authentic
  
-  validates_presence_of :login, :email, :first_name
-  validates_uniqueness_of :login, :email
+  #validates_presence_of :login, :email, :first_name
+  #validates_uniqueness_of :login, :email
   
-  def name
-    "#{self.first_name} #{self.last_name}".strip
-  end
+  #def name
+  #  "#{self.first_name} #{self.last_name}".strip
+  #end
  
   # Import user from Basecamp
-  def self.import(h)
-    u = User.new(
-      :login => h['user_name'],
-      :email => h['email_address'],
-      :password => h['password'],
-      :password_confirmation => h['password'],
-      :first_name => h['first_name'],
-      :last_name => h['last_name']
-    )
-    u.save
-    u
-  end
+  #def self.import(h)
+  #  u = User.new(
+  #    :login => h['user_name'],
+  #    :email => h['email_address'],
+  #    :password => h['password'],
+  #    :password_confirmation => h['password'],
+  #    :first_name => h['first_name'],
+  #    :last_name => h['last_name']
+  #  )
+  #  u.save
+  #  u
+  #end
 end
